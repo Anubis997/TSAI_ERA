994cdbb7acca949e3699842c9c57a22a2186cde2
Ajay Kumar Nadimpalli <96742087+Anubis997@users.noreply.github.com>
2024-07-16 20:44:45 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,48 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import OneCycleLR
+import torchvision.datasets as datasets
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+from torch_lr_finder import LRFinder
+import torch.optim as optim
+
+
+# Define transformations
+# Train Phase transformations
+
+def get_train_test_transform():
+    train_transforms = transforms.Compose([
+        transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+        transforms.RandomApply([transforms.RandomCrop(size=(32, 32), padding=8)], p=0.5)  # Applying CutOut with 1 hole of size 8x8
+    ])
+
+    # Test Phase transformations
+    test_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
+    ])
+
+
+def plotting_train_test_accuracy(train_acc,train_losses,test_acc,test_losses):
+    t = [t_items.item() for t_items in train_losses]
+    %matplotlib inline
+    import matplotlib.pyplot as plt
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(t)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc[4000:])
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
+
+
